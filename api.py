94e466386afdf4f0757dc6d628a3be5658163146
julien domagalski <julien.domagalski@gmail.com>
2021-08-23 12:05:30 +0200

    awesome stuff

--- api.py
+++ api.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return "Hello from Cloud Run CD"
+
+
+@app.get("/addition")
+def addition(first, second):
+    return f"{first} + {second} = {first + second}"
